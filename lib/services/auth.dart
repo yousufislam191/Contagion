d385c7ff1ebe5756de5b32d12e46106126df076e
yousufislam <yousufislam337@gmail.com>
2022-02-05 02:56:45 +0600

    some chnges

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -5,6 +5,7 @@ import 'package:lu_ahatting_application/services/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  String? errorMessage;
 
   // register with email and password
   Future registerWithEmailAndPassword(String _email, String _conPass,
@@ -25,4 +26,38 @@ class AuthService {
       return null;
     }
   }
+
+// // for login
+//   Future loginWithEmailAndPassword(String _email, String _pass) async {
+//     try {
+//       final newUser = await _auth.signInWithEmailAndPassword(
+//           email: _email, password: _pass);
+//       login_user_data(newUser);
+//     } on FirebaseAuthException catch (error) {
+//       switch (error.code) {
+//         case "invalid-email":
+//           errorMessage = "Your email address appears to be incorrect.";
+//           break;
+//         case "wrong-password":
+//           errorMessage = "Your password is wrong.";
+//           break;
+//         case "user-not-found":
+//           errorMessage = "User with this email doesn't exist.";
+//           break;
+//         case "user-disabled":
+//           errorMessage = "User with this email has been disabled.";
+//           break;
+//         case "too-many-requests":
+//           errorMessage = "Too many requests";
+//           break;
+//         case "operation-not-allowed":
+//           errorMessage = "Signing in with Email and Password is not enabled.";
+//           break;
+//         default:
+//           errorMessage = "An undefined Error happened.";
+//       }
+//       Fluttertoast.showToast(msg: errorMessage!);
+//       print(error.code);
+//     }
+//   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -22,43 +22,156 @@ user_data(String name, String id, var departmentType, var identityType) async {
   usermodel.department = _departmentType;
   usermodel.identity = _identityType;
 
-  //for student
-  if (_identityType == 'Student') {
-    await firebaseFirestore
-        .collection('Student')
-        .doc(usermodel.uid)
-        .set(usermodel.toMap());
-    Fluttertoast.showToast(
-        msg: "Account created successfully",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIosWeb: 1,
-        fontSize: 16.0);
-  }
-  //for Teacher
-  else if (_identityType == 'Teacher') {
-    await firebaseFirestore
-        .collection('Teacher')
-        .doc(usermodel.uid)
-        .set(usermodel.toMap());
-    Fluttertoast.showToast(
-        msg: "Account created successfully",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIosWeb: 1,
-        fontSize: 16.0);
-  }
-  //for Dept. Head
-  else if (_identityType == 'Dept. Head') {
-    await firebaseFirestore
-        .collection('Dept. Head')
-        .doc(usermodel.uid)
-        .set(usermodel.toMap());
-    Fluttertoast.showToast(
-        msg: "Account created successfully",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIosWeb: 1,
-        fontSize: 16.0);
+  // //for student
+  // if (_identityType == 'Student') {
+  //   await firebaseFirestore
+  //       .collection('Student')
+  //       .doc(usermodel.uid)
+  //       .set(usermodel.toMap());
+  //   Fluttertoast.showToast(
+  //       msg: "Account created successfully",
+  //       toastLength: Toast.LENGTH_SHORT,
+  //       gravity: ToastGravity.CENTER,
+  //       timeInSecForIosWeb: 1,
+  //       fontSize: 16.0);
+  // }
+  // //for Teacher
+  // else if (_identityType == 'Teacher') {
+  //   await firebaseFirestore
+  //       .collection('Teacher')
+  //       .doc(usermodel.uid)
+  //       .set(usermodel.toMap());
+  //   Fluttertoast.showToast(
+  //       msg: "Account created successfully",
+  //       toastLength: Toast.LENGTH_SHORT,
+  //       gravity: ToastGravity.CENTER,
+  //       timeInSecForIosWeb: 1,
+  //       fontSize: 16.0);
+  // }
+  // //for Dept. Head
+  // else if (_identityType == 'Dept. Head') {
+  //   await firebaseFirestore
+  //       .collection('Dept. Head')
+  //       .doc(usermodel.uid)
+  //       .set(usermodel.toMap());
+  //   Fluttertoast.showToast(
+  //       msg: "Account created successfully",
+  //       toastLength: Toast.LENGTH_SHORT,
+  //       gravity: ToastGravity.CENTER,
+  //       timeInSecForIosWeb: 1,
+  //       fontSize: 16.0);
+  // }
+
+  List<String> dept = List.filled(11, 'null'); // declare a String type List
+  dept[0] = 'BBA';
+  dept[1] = 'CSE';
+  dept[2] = 'English';
+  dept[3] = 'EEE';
+  dept[4] = 'Civil Engineering';
+  dept[5] = 'Architecture';
+  dept[6] = 'Law';
+  dept[7] = 'Islamic Studies';
+  dept[8] = 'Public Health';
+  dept[9] = 'Tourism & Hospitality Management';
+  dept[10] = 'Bangla';
+
+  List<String> identity = List.filled(3, 'null'); // declare a String type List
+  identity[0] = 'Student';
+  identity[1] = 'Teacher';
+  identity[2] = 'Dept. Head';
+
+  for (int i = 0; i < 11; i++) {
+    String _dept = dept[i];
+    if (_departmentType == _dept) {
+      for (int i = 0; i < 3; i++) {
+        String _identity = identity[i];
+        if (_identityType == _identity) {
+          await firebaseFirestore
+              .collection(_dept)
+              .doc(usermodel.uid)
+              .collection(_identity)
+              .doc(usermodel.uid)
+              .set(usermodel.toMap());
+          Fluttertoast.showToast(
+              msg: "Account created successfully",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.CENTER,
+              timeInSecForIosWeb: 1,
+              fontSize: 16.0);
+        }
+      }
+    }
   }
 }
+
+// login_user_data(UserCredential newUser) async {
+//   // const login_user_data({Key? key, required this._value}) : super(key: key);
+
+//   String _identityValue;
+//   // login_user_data({this._identityValue});
+
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//   // if login successfull then go to the if condition
+//   if (newUser != null) {
+//     final user = await _auth
+//         .currentUser!; // when user login with their email & pass then will find the current user
+//     final userID = user.uid; // get current user 'uid'
+
+//     List<String> dept = List.filled(11, 'null'); // declare a String type List
+//     dept[0] = 'BBA';
+//     dept[1] = 'CSE';
+//     dept[2] = 'English';
+//     dept[3] = 'EEE';
+//     dept[4] = 'Civil Engineering';
+//     dept[5] = 'Architecture';
+//     dept[6] = 'Law';
+//     dept[7] = 'Islamic Studies';
+//     dept[8] = 'Public Health';
+//     dept[9] = 'Tourism & Hospitality Management';
+//     dept[10] = 'Bangla';
+
+//     List<String> identity =
+//         List.filled(3, 'null'); // declare a String type List
+//     identity[0] = 'Student';
+//     identity[1] = 'Teacher';
+//     identity[2] = 'Dept. Head';
+
+//     for (int i = 0; i < 11; i++) {
+//       String _dept = dept[i];
+//       for (int i = 0; i < 3; i++) {
+//         // we use loop for matchin collection with database
+//         String _identity = identity[i]; // convert List Srting to String
+//         try {
+//           // when user login then it will find user identity from database & keep the identity value in '_value' variable
+//           DocumentSnapshot value = await FirebaseFirestore.instance
+//               .collection(_dept)
+//               .doc(userID)
+//               .collection(_identity)
+//               .doc(userID)
+//               .get();
+//           _identityValue = (value['identity']); // keep the identity value
+
+//           // void get_identity_value() {
+//           //   print(_identityValue);
+//           //   String getValue = _identityValue;
+//           // }
+
+//           // login_user_data({this._identityValue});
+
+//           // var get_identity_value = new get_identity_value();
+//           break;
+//         } catch (e) {
+//           print(e);
+//         }
+//       }
+//     }
+//   }
+// }
+// // class get_identity_value {
+// //   // var _identityValue;
+
+// //   login_user_data test1 = new login_user_data();
+
+// //   login_user_data._identityValue;
+// // }
